103b7ee37551d63427c9e49bb434053d652d2f00
vi <79205177+im-vi@users.noreply.github.com>
2022-11-18 18:11:12 +0100

    re

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(express.static(__dirname + '/public'))
 
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + 'public/html/index.html')
+    res.sendFile(__dirname + '/index.html')
 })
 
 app.get('/ogladaj', function(req, res) {
