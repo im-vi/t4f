368af83e0d56ab05c910e9315557a43dac6a7eea
vi <79205177+im-vi@users.noreply.github.com>
2022-11-18 18:08:42 +0100

    hi

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ app.use(express.static(__dirname + '/public'))
 
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html')
+    res.sendFile(__dirname + 'public/html/index.html')
 })
 
 app.get('/ogladaj', function(req, res) {
-    res.sendFile(__dirname + '/ogladaj.html')
+    res.sendFile(__dirname + 'public/html/ogladaj.html')
 })
 
 app.listen('4000', function() {
