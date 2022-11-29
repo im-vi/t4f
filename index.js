8f3b531f035a8420462bb4bc3e481e1c6f1fe92f
vi <79205177+im-vi@users.noreply.github.com>
2022-11-29 23:22:10 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ app.get('/turnieje', function(req, res) {
     res.sendFile(__dirname + '/turnieje.html')
 })
 
+app.get('/o-nas', function(req, res) {
+    res.sendFile(__dirname + '/o-nas.html')
+})
+
 app.listen('4000', function() {
     console.log('Server is running!')
 })
